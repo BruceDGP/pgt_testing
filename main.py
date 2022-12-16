b7263a9bb04f54a17efa3f5683602b5145c7925e
Bruce <brucedgp@gmail.com>
2022-12-16 13:41:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ os.system("pip install nextcord")
 
 import nextcord #importamos para conectarnos con el bot
 from nextcord.ext import commands #importamos los comandos
-import datetime
-import random
 
 bot = commands.Bot(command_prefix="t", description="PGT testing", help_command=None, intents=nextcord.Intents.all())
 
@@ -17,3 +15,5 @@ async def on_ready():
         await bot.get_user(458428946963496960).send("El bot se acaba de encender")
     except :
         pass
+
+bot.run(os.getenv("TOKEN"))
