f0a9f8e658da5fb4a57c8b54bc840afb60ba9a51
Bruce <brucedgp@gmail.com>
2022-12-16 13:20:12 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+os.system("pip install nextcord")
+
+import nextcord #importamos para conectarnos con el bot
+from nextcord.ext import commands #importamos los comandos
+import datetime
+import random
+
+bot = commands.Bot(command_prefix="t", description="PGT testing", help_command=None, intents=nextcord.Intents.all())
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=nextcord.Game(name="probanding..."))
+    print('El bot está listo!')
+    try:
+        await bot.get_user(458428946963496960).send("El bot se acaba de encender")
+    except :
+        pass
