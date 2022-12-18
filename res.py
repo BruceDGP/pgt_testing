e49c5e81d4ad4823162eccf6aac486224d9a795b
Bruce <brucedgp@gmail.com>
2022-12-18 12:27:26 -0500

    Update res.py

--- res.py
+++ res.py
@@ -3,3 +3,11 @@ import requests
 response = requests.get('https://httpbin.org/get')
 
 response.status_code
+
+def check(resp):
+    if resp:
+        return True
+    else
+        return True
+
+check(response)
