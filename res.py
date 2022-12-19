92f02b99448754877ec4937b5871ca9c1a1cccb5
Bruce <brucedgp@gmail.com>
2022-12-18 21:49:54 -0500

    Update res.py

--- res.py
+++ res.py
@@ -1,15 +1,3 @@
-import requests
-
-response = requests.get('https://httpbin.org/get')
-
-response.status_code
-
-def check(resp):
-    if resp:
-        print(resp.status_code)
-        return resp.status_code
-    else:
-        return resp.status_code
-
-check(response)
-response.status_code
+import sys
+# calculate and stuff
+sys.exit(200)
