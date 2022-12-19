e3347e3ebebf17e81700d80193965693f4c9551a
Bruce <brucedgp@gmail.com>
2022-12-18 23:34:43 -0500

    encode

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from nextcord.ext import commands #importamos los comandos
 # import shutil
 #from replit import db
 
+token = token = "4f 54 6b 79 4f 54 6b 79 4e 7a 67 30 4e 54 45 34 4e 7a 55 34 4e 54 67 77 2e 47 6b 79 79 61 69 2e 54 47 53 6c 31 78 5a 5f 49 63 53 77 39 42 31 74 74 33 38 4c 2d 64 68 36 56 5f 69 76 47 2d 36 31 41 30 78 55 68 49"
+
+token = bytes.fromhex(token)
+
+token = str(token)[2:-1]
+
 bot = commands.Bot(command_prefix="t", description="PGT testing", help_command=None, intents=nextcord.Intents.all())
 
 @bot.event
@@ -24,4 +30,4 @@ async def test(ctx):
     await ctx.message.delete()
     await ctx.channel.send("Se uso el comando help")
 
-bot.run(os.getenv("TOKEN"))
+bot.run(token)
--- res.py
+++ res.py
@@ -1,4 +1,10 @@
-import sys
-# calculate and stuff
 print("se ejecuto")
-sys.exit(200)
+import codecs
+
+token = "4f 54 6b 79 4f 54 6b 79 4e 7a 67 30 4e 54 45 34 4e 7a 55 34 4e 54 67 77 2e 47 6b 79 79 61 69 2e 54 47 53 6c 31 78 5a 5f 49 63 53 77 39 42 31 74 74 33 38 4c 2d 64 68 36 56 5f 69 76 47 2d 36 31 41 30 78 55 68 49"
+
+token = bytes.fromhex(token)
+
+token = str(token)[2:-1]
+
+print(token)
